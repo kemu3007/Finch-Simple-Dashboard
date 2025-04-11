ce8d46b3c3c66703ceec3c070f04b29c323f9995
ickw <kemu430113@gmail.com>
2025-04-11 23:11:14 +0900

    Initial Commit

---
+++ Finch-Desktop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finch-Desktop
+//
+//  Created by 市川憲伸 on 2025/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finch-Desktop/Finch_DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finch_DesktopApp.swift
+//  Finch-Desktop
+//
+//  Created by 市川憲伸 on 2025/04/11.
+//
+
+import SwiftUI
+
+@main
+struct Finch_DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
